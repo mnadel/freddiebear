b211d36f82d3b0008d3da6c0a49d83533e790c8f
Michael Nadel <mike@nadel.org>
2022-02-21 13:15:37 -0600

    moved root cmd to main

--- main.go
+++ main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"log"
 
-	"github.com/mnadel/bearfred/cmd"
+	"github.com/mnadel/bearfred/cmd/journal"
+	"github.com/mnadel/bearfred/cmd/search"
+	"github.com/mnadel/bearfred/cmd/version"
+	"github.com/spf13/cobra"
 )
 
 func main() {
+	cmd := &cobra.Command{
+		Use:   "bearfred",
+		Short: "A CLI for an Alfred+Bear integration",
+		Long:  "Search notes, plus helpers to implement a daily journal",
+	}
+
+	cmd.AddCommand(journal.New())
+	cmd.AddCommand(search.New())
+	cmd.AddCommand(version.New())
+
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
