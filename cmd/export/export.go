b822f991ef94eb6ee40fd91103b277bb52631c3a
Michael Nadel <mike@nadel.org>
2022-03-24 09:06:34 -0500

    refactored exporter to own package

--- cmd/export/export.go
+++ cmd/export/export.go
@@ -1,30 +1,20 @@
 package export
 
 import (
-	"crypto/md5"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"path"
-	"regexp"
 
 	"github.com/mnadel/freddiebear/db"
+	"github.com/mnadel/freddiebear/db/export"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-)
-
-const (
-	FilenameTemplate = "%s (%s).md"
-	FilenameRegex    = `.*\s\((\w+)\)\.md$`
 )
 
 var (
 	listOnly bool
 )
-
-type SHA string
-type Filename string
 
 func New() *cobra.Command {
 	searchCmd := &cobra.Command{
@@ -68,28 +58,26 @@ func runner(cmd *cobra.Command, args []string) error {
 
 func printingExporter(destinationDir string) db.Exporter {
 	return func(record *db.Record) error {
-		fmt.Println(path.Join(destinationDir, buildFilename(record)))
+		fmt.Println(path.Join(destinationDir, export.BuildFilename(record)))
 		return nil
 	}
 }
 
 func writingExporter(destinationDir string) (db.Exporter, error) {
-	filenameSHAs, err := getFilenameSHAs(destinationDir)
+	exporter, err := export.NewExporter(destinationDir)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
 
 	return func(record *db.Record) error {
-		filename := path.Join(destinationDir, buildFilename(record))
-		recordText := []byte(record.Text)
+		if renamed, oldName := exporter.IsRenamed(record); renamed {
+			log.Println("detected rename of", oldName)
 
-		if renamed, oldName := wasRenamed(SHA(record.SHA), filename, filenameSHAs); renamed {
-			log.Println("removing renamed file", oldName, "->", filename)
 			if err := os.Remove(string(oldName)); err != nil {
 				return errors.WithStack(err)
 			}
 		} else {
-			changes, err := hasChanges(record.SHA, recordText, filenameSHAs)
+			changes, err := exporter.IsChanged(record)
 			if err != nil {
 				return errors.WithStack(err)
 			} else if !changes {
@@ -97,63 +85,18 @@ func writingExporter(destinationDir string) (db.Exporter, error) {
 			}
 		}
 
-		log.Println("exporting", record.SHA)
-
-		if err := os.WriteFile(filename, recordText, 0644); err != nil {
-			return errors.WithStack(err)
-		}
-
-		return nil
+		log.Println("exporting", record.SHA, record.Title)
+		return writeRecord(record, destinationDir)
 	}, nil
 }
 
-func buildFilename(record *db.Record) string {
-	return fmt.Sprintf(FilenameTemplate, record.Title, record.SHA)
-}
+func writeRecord(record *db.Record, destinationDir string) error {
+	filename := path.Join(destinationDir, export.BuildFilename(record))
+	recordText := []byte(record.Text)
 
-func hasChanges(sha string, newData []byte, mapping map[SHA]Filename) (bool, error) {
-	filename, ok := mapping[SHA(sha)]
-	if !ok {
-		return true, nil
+	if err := os.WriteFile(filename, recordText, 0644); err != nil {
+		return errors.WithStack(err)
 	}
 
-	oldData, err := os.ReadFile(string(filename))
-	if err != nil {
-		return false, errors.WithStack(err)
-	}
-
-	oldSum := md5.Sum(oldData)
-	newSum := md5.Sum(newData)
-
-	return oldSum != newSum, nil
-}
-
-func wasRenamed(sha SHA, newName string, mapping map[SHA]Filename) (bool, Filename) {
-	f, ok := mapping[sha]
-	if !ok {
-		return false, ""
-	}
-
-	return string(f) != newName, f
-}
-
-func getFilenameSHAs(directory string) (map[SHA]Filename, error) {
-	files, err := ioutil.ReadDir(directory)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	filenames := make(map[SHA]Filename)
-	re := regexp.MustCompile(FilenameRegex)
-
-	for _, file := range files {
-		if !file.IsDir() {
-			parts := re.FindStringSubmatch(file.Name())
-			if len(parts) == 2 {
-				filenames[SHA(parts[1])] = Filename(file.Name())
-			}
-		}
-	}
-
-	return filenames, nil
+	return nil
 }
---
+++ db/export/exporter.go
@@ -0,0 +1,78 @@
+package export
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+
+	"github.com/mnadel/freddiebear/db"
+	"github.com/pkg/errors"
+)
+
+const (
+	FilenameTemplate = "%s (%s).md"
+	FilenameRegex    = `.*\s\((\w+)\)\.md$`
+)
+
+type SHA string
+type Filename string
+
+type Exporter struct {
+	mapping   map[SHA]Filename
+	directory string
+}
+
+func NewExporter(directory string) (*Exporter, error) {
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	filenames := make(map[SHA]Filename)
+	re := regexp.MustCompile(FilenameRegex)
+
+	for _, file := range files {
+		if !file.IsDir() {
+			parts := re.FindStringSubmatch(file.Name())
+			if len(parts) == 2 {
+				filenames[SHA(parts[1])] = Filename(file.Name())
+			}
+		}
+	}
+
+	return &Exporter{filenames, directory}, nil
+}
+
+// Returns true if the SHA and its new data differs from the previously-exported contents
+func (e *Exporter) IsChanged(record *db.Record) (bool, error) {
+	filename, ok := e.mapping[SHA(record.SHA)]
+	if !ok {
+		return true, nil
+	}
+
+	oldData, err := os.ReadFile(string(filename))
+	if err != nil {
+		return false, errors.WithStack(err)
+	}
+
+	oldSum := md5.Sum(oldData)
+	newSum := md5.Sum([]byte(record.Text))
+
+	return oldSum != newSum, nil
+}
+
+// Returns true if the SHA has been renamed, and if so, what the previous name was
+func (e *Exporter) IsRenamed(record *db.Record) (bool, Filename) {
+	f, ok := e.mapping[SHA(record.SHA)]
+	if !ok {
+		return false, ""
+	}
+
+	return string(f) != BuildFilename(record), f
+}
+
+func BuildFilename(record *db.Record) string {
+	return fmt.Sprintf(FilenameTemplate, record.Title, record.SHA)
+}
