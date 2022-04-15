dd78afef5311b87741ad5e3c4c77e75667bce639
Michael Nadel <mike@nadel.org>
2022-04-15 16:16:58 -0500

    use x-fb-xyz as a token to indicate it's a freddiebear extension

--- cmd/search/search.go
+++ cmd/search/search.go
@@ -48,48 +48,61 @@ func runner(cmd *cobra.Command, args []string) error {
 		return errors.WithStack(err)
 	}
 
-	fmt.Print(serialize(results, args[0]))
+	if len(results) == 0 {
+		fmt.Print(serializeCreate(args[0]))
+	} else {
+		fmt.Print(serializeOpen(results))
+	}
 
 	return nil
 }
 
-func serialize(results db.Results, searchTerm string) string {
+func serializeCreate(searchTerm string) string {
 	builder := strings.Builder{}
 
 	builder.WriteString(`<?xml version="1.0" encoding="utf-8"?>`)
 	builder.WriteString(`<items>`)
 
-	if len(results) == 0 {
+	builder.WriteString(`<item valid="yes">`)
+	builder.WriteString(`<subtitle>Create note</subtitle>`)
+	builder.WriteString(`<title>`)
+	builder.WriteString(searchTerm)
+	builder.WriteString(`</title>`)
+	builder.WriteString(`<arg>`)
+	builder.WriteString(`x-fb-create:`)
+	builder.WriteString(searchTerm)
+	builder.WriteString(`</arg>`)
+	builder.WriteString(`</item>`)
+
+	builder.WriteString(`</items>`)
+
+	return builder.String()
+}
+
+func serializeOpen(results db.Results) string {
+	builder := strings.Builder{}
+
+	builder.WriteString(`<?xml version="1.0" encoding="utf-8"?>`)
+	builder.WriteString(`<items>`)
+
+	for _, item := range results {
 		builder.WriteString(`<item valid="yes">`)
-		builder.WriteString(`<subtitle>Create note</subtitle>`)
 		builder.WriteString(`<title>`)
-		builder.WriteString(searchTerm)
+		builder.WriteString(item.Title)
 		builder.WriteString(`</title>`)
+
+		if !optShowTags {
+			builder.WriteString(`<subtitle>Open note</subtitle>`)
+		} else {
+			builder.WriteString(`<subtitle>`)
+			builder.WriteString(strings.Join(item.UniqueTags(), ", "))
+			builder.WriteString(`</subtitle>`)
+		}
+
 		builder.WriteString(`<arg>`)
-		builder.WriteString(`create:`)
-		builder.WriteString(searchTerm)
+		builder.WriteString(item.ID)
 		builder.WriteString(`</arg>`)
 		builder.WriteString(`</item>`)
-	} else {
-		for _, item := range results {
-			builder.WriteString(`<item valid="yes">`)
-			builder.WriteString(`<title>`)
-			builder.WriteString(item.Title)
-			builder.WriteString(`</title>`)
-
-			if !optShowTags {
-				builder.WriteString(`<subtitle>Open note</subtitle>`)
-			} else {
-				builder.WriteString(`<subtitle>`)
-				builder.WriteString(strings.Join(item.UniqueTags(), ", "))
-				builder.WriteString(`</subtitle>`)
-			}
-
-			builder.WriteString(`<arg>`)
-			builder.WriteString(item.ID)
-			builder.WriteString(`</arg>`)
-			builder.WriteString(`</item>`)
-		}
 	}
 
 	builder.WriteString(`</items>`)
