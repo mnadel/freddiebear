6fa2f3c72d5864ec86e12a3a2fab44b9890ad545
Michael Nadel <mike@nadel.org>
2022-04-18 11:21:43 -0500

    fix x-fb key generation

--- ext/ext.go
+++ ext/ext.go
@@ -3,12 +3,12 @@ package ext
 import "strings"
 
 const (
-	// X_FREDDIEBEAR is the freddbear extension prefix.
-	X_FREDDIEBEAR = `x-fb`
+	// X_FREDDIEBEAR is the freddiebear extension prefix.
+	X_FREDDIEBEAR = "x-fb"
 )
 
-// KeyValue returns an extension-specific key and value.
-func KeyValue(key, value string) string {
+// CreateKeyValue creates an extension-specific key and value string.
+func CreateKeyValue(key, value string) string {
 	b := strings.Builder{}
 
 	WriteKeyValue(b, key, value)
@@ -19,6 +19,7 @@ func KeyValue(key, value string) string {
 // KeyValue write an extension-specific key and value to the given Builder.
 func WriteKeyValue(b strings.Builder, key string, value string) {
 	b.WriteString(X_FREDDIEBEAR)
+    b.WriteString("-")
 	b.WriteString(key)
 	b.WriteString(":")
 	b.WriteString(value)
